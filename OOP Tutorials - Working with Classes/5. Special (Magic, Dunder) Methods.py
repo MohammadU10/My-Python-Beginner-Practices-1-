bcd97ed9f08c748854ae2a430e0a22e82155af9c
Ninja.U102 <120134432+NinjaU10@users.noreply.github.com>
2023-09-05 13:25:08 +0330

    Update 5. Special (Magic, Dunder) Methods.py

--- OOP Tutorials - Working with Classes/5. Special (Magic, Dunder) Methods.py
+++ OOP Tutorials - Working with Classes/5. Special (Magic, Dunder) Methods.py
@@ -48,7 +48,7 @@ print(emp_1) """
 
 
 # 1.3. So Let's take a look at Some Other Common Special Methods :
-class Employee:
+""" class Employee:
     
     raise_amt = 1.04
     
@@ -64,7 +64,7 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amt)
     
-# So 2 More Common Special Methods that we should Probably Always Implement Are '__repr__' And '__str__' :
+    # So 2 More Common Special Methods that we should Probably Always Implement Are '__repr__' And '__str__' :
     # 1) In Short, '__repr__' is Meant to be Unambiguous Representation of the Object,
     # And Should be Used for Debugging And Logging And things Like that, It's Really Meant to be Seen By Other Developers :
     def __repr__(self):
@@ -84,7 +84,7 @@ class Employee:
 
 
 emp_1 = Employee('Corey', 'Schafer', 50000)
-emp_2 = Employee('Test', 'Employee', 60000)
+emp_2 = Employee('Test', 'Employee', 60000) """
 
 # Now These 2 Are What Implicitly Called Anytime we Run 'repr' on One of our Objects OR 'str' on One of our Objects,
 # And These Are What we're going to Use to Fix our Problem of Printing out the Vague Employee Object,
@@ -136,3 +136,127 @@ print(emp_1.__str__()) """
 # * Note: That's Kind of a Contrived Example, Because if we were to Make a Class Like that in Real Life,
 # Then it's Probably Better to be Explicit About What you're Adding Together,
 # But just for the sake of this Example, Let's go ahead and see How we do this. *
+
+""" class Employee:
+    
+    raise_amt = 1.04
+    
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.email = first + '.' + last + '@email.com'
+        self.pay = pay
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+    
+    def __repr__(self):
+        return "Employee('{}', '{}', '{}')".format(self.first, self.last, self.pay)
+    
+    def __str__(self):
+        return "{} - {}".format(self.fullname(), self.email)
+    
+    # Creating our Custom '__add__' Method :
+    def __add__(self, other):
+        return self.pay + other.pay
+
+
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Test', 'Employee', 60000)
+
+# Using our Custom '__add__' Method :
+print(emp_1 + emp_2) """
+# If we Run this, we can see that When we Added these 2 Employee Objects Together,
+# It gave us their Combined Salaries.
+
+# Now if we Didn't have this '__add__' Method, And we Comment out that, If we try to run that,
+# Then it gives us an Error And it says that It Doesn't Know How to Add these 2 Employees Together.
+
+
+
+# 1.6. Now There are All Kinds of Special Methods for Arithmetic that We can See and Explore in the Python Documentation :
+
+# There are Special Methods For Subtracting, Multiplying, Dividing And plenty of others.
+
+# * Topic in the Python Documentation: 3.3.7. "Emulating numeric types" *
+
+
+
+# 1.7. One More Example :
+
+# Now the 'len()' Function that we use to Check the Length of a List Or a String, Now This is Also a Special Method;
+# So if we wanted to Print the Length of a String, For Example :
+""" print(len('test')) """
+# We can see that this String is 4 Characters Long;
+
+# Now this is Also Using a Special Dunder Method in the Background :
+""" print('test'.__len__()) """
+# We can see that we get the Same Result.
+
+
+# So if We want This 'len()' Function to Work On our Employee Class Objects,
+# Then We have to Create a '__len__' Method;
+
+# So Let's say that For Example, When we Ran 'len' On our Employee Instance,
+# That We wanted to Return the Total Number of Characters And Their Full Name;
+# And Maybe this could be Useful if Someone is Writing a Document,
+# And Needs to Know How Many Characters the Employee's Name will take up :
+
+class Employee:
+    
+    raise_amt = 1.04
+    
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.email = first + '.' + last + '@email.com'
+        self.pay = pay
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+    
+    def __repr__(self):
+        return "Employee('{}', '{}', '{}')".format(self.first, self.last, self.pay)
+    
+    def __str__(self):
+        return "{} - {}".format(self.fullname(), self.email)
+    
+    def __add__(self, other):
+        return self.pay + other.pay
+    
+    # Creating our Custom '__len__' Method :
+    def __len__(self):
+        return len(self.fullname())
+
+
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Test', 'Employee', 60000)
+
+# Using our Custom '__len__' Method On Our Objects :
+print(len(emp_1))
+print(len(emp_2))
+
+
+# Now there a Ton of Other Special Methods that we could go over;
+# So we can these to Customize How Objects Are Compared, How They Check For Equality,
+# And a Lot of Other Useful Stuff that we're Not able to Fit into One Video/File;
+
+# But if We go to Python Documentation,
+# Then we can see a Short Description of All the Ones that we can use :
+# Link: "https://docs.python.org/3/reference/datamodel.html#special-method-names"
+
+
+
+# 1.8. In the Real-World Examples in the Standard Library (Here in 'datetime' Module in its '__add__' Method),
+# We can see that Some Functions Return 'NotImplemented',
+# Basically When They Return 'NotImplemented', They Don't Want to Throw an Error,
+# Because the Other Object might Know How to Handle that Operation;
+
+# So Returning 'NotImplemented', is a Way to Fall Back On the Other Object to See if it Knows How to Handle the Operation,
+# And if None of them Know How to Handle it, Then it will Eventually Throw an Error.
